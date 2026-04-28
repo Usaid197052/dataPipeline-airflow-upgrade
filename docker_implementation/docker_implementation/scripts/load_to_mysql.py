908b886e6e2131d37f386bd52aaa6b5a6521e664
Usaid Ahmed <usaid33376@gmail.com>
2026-04-28 10:19:10 +0500

    Refactor load_to_mysql.py for improved readability

--- docker_implementation/docker_implementation/scripts/load_to_mysql.py
+++ docker_implementation/docker_implementation/scripts/load_to_mysql.py
@@ -8,21 +8,14 @@ connection = pymysql.connect(
     host="host.docker.internal",
     user="root",
     password="usaid123",
-    database="week3_db"
-)
+    database="week3_db")
 
 cursor = connection.cursor()
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
-
 processed_path = os.path.join(PROJECT_ROOT, "data", "processed", "cleaned_api_data.csv")
-
-
 df = pd.read_csv(processed_path)
-
 print("Data loaded.")
-
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS api_data (
@@ -34,7 +27,6 @@ CREATE TABLE IF NOT EXISTS api_data (
 )
 """)
 cursor.execute("TRUNCATE TABLE api_data")
-
 for _, row in df.iterrows():
     cursor.execute("""
         INSERT INTO api_data (userId, id, title, body, title_length)
@@ -46,9 +38,7 @@ for _, row in df.iterrows():
         row['body'],
         int(row['title_length'])
     ))
-
 connection.commit()
-
 print("Data loaded into MySQL successfully.")
 
 cursor.close()
